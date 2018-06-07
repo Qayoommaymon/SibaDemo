c1676f70bec4e2e1c0858e04c6e7e2c91a74d149
Qayoommaymon <40027075+Qayoommaymon@users.noreply.github.com>
2018-06-07 16:56:49 +0500

    Initial

---
+++ WebAppBasicDemo/WebAppBasicDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppBasicDemo.Startup))]
+namespace WebAppBasicDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
